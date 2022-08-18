9209b028fe5b742544fd183fd3e9e184c4be1a49
Richard Project Manager <106099024+RichardQuant@users.noreply.github.com>
2022-08-17 23:39:51 -0400

    Initial commit

---
+++ .github/pull_request_template.md
@@ -0,0 +1,13 @@
+This PR...
+
+## Changes
+-
+
+## Screnshots
+
+## Checklist
+- [ ] tested locally
+- [ ] updated the docs
+- [ ] added tests
+
+Fixes #
---
+++ main.py
@@ -0,0 +1,22 @@
+def run():
+    """
+    Please keep everything clear, no hard rules please be inspired by the
+    following.
+    https://dev.to/danialmalik/a-brief-guide-to-clean-code-functions-104h
+    https://realpython.com/python-refactoring/
+    """
+    # Testing PR
+    print('run started...')
+    # Put procedures and sub procedures here, if you want. Maybe use the step down rule.
+    print('run ended')
+
+
+if __name__ == '__main__':
+    print('V 1.01 started...')
+    # Please, put some tests here
+    print('Tests passed.')
+    run()
+    import os
+ 
+    # printing environment variables
+    print(os.environ)
